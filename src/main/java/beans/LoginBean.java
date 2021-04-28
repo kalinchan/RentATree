7d83254502f06ae86d2a85161356c5aaac7d8d2c
Kalin Chan <kalin.chan@payara.fish>
2021-04-28 16:11:31 +0100

    PrimeFaces

---
+++ src/main/java/beans/LoginBean.java
@@ -0,0 +1,63 @@
+package beans;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+
+import dao.LoginDAO;
+
+@Named
+@SessionScoped
+public class LoginBean implements Serializable {
+
+	private static final long serialVersionUID = 6955508471291131930L;
+
+	private String email;
+	private String password;
+	
+	@Inject
+	LoginDAO loginDAO;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String validateLogin() {
+		boolean valid = loginDAO.validate(email, password);
+
+		if (valid) {
+			HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext()
+					.getSession(false);
+			session.setAttribute("email", email);
+			return "home";
+		}
+
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_WARN, "Incorrect Email or Password", "Try again"));
+		return "null";
+	}
+	
+	public String logout() {
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext()
+				.getSession(false);
+		session.invalidate();
+		return "login";
+	}
+}
---
+++ src/main/java/dao/LoginDAO.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.Dependent;
+import javax.sql.DataSource;
+
+import org.jboss.logging.Logger;
+
+@Dependent
+public class LoginDAO implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+
+	private static Logger LOGGER = Logger.getLogger(LoginDAO.class.getName());
+
+	@Resource(lookup ="jdbc/mariadbpool")
+	DataSource dataSource;
+
+	public boolean validate(String email, String password) {
+		try (Connection connection = dataSource.getConnection()) {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("SELECT Email, Password FROM Customer WHERE Email=? AND Password=?");
+			preparedStatement.setString(1, email);
+			preparedStatement.setString(2, password);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			if(resultSet.next()) {
+				return true;
+			}
+			return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			LOGGER.info("SQLException: " + e);
+			return false;
+		}
+	}
+}
